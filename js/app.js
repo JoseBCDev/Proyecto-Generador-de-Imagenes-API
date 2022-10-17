8b31be28c1eff4b6055d6dd0f6f99cc1fc1d9b30
xdgonex <bendezucortezjoseluis@gmail.com>
2022-10-17 05:36:01 -0500

    Mostrando en el html las imagenes buscadas por el formulario

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ function mostrarAlerta(mensaje)
 function buscarImagenes(palabra)
 {
     const key = '30646724-28b6ce9c335c39d50d52bf7f8';
-    const url = `https://pixabay.com/api/?key=${key}&q=${palabra}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${palabra}&per_page=100`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -62,5 +62,23 @@ function buscarImagenes(palabra)
 
 function mostrarImagenes(imagenes)
 {
-    console.log(imagenes);
+    imagenes.forEach(imagen => {
+        const {likes,previewURL,largeImageURL,views} = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+                    <div class="p-4">
+                        <p class="font-bold">${likes}<span class="font-light">Me gusta</span></p>
+                        <p class="font-bold">${views}<span class="font-light">Veces Vista</span></p>
+                        <a 
+                            class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                            href="${largeImageURL}" target="_blank rel="noopener noreferrer"
+                        >Ver imagen</a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
 }
